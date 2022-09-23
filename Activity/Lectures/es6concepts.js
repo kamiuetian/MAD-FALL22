429948e62920b274ca0e4cd0096ec13b1e4cf0f0
kamiuetian <kami_se01@live.com>
2022-09-23 13:51:00 +0500

    Lab Tasks BSE 23-09-2022

--- Activity/Lectures/es6concepts.js
+++ Activity/Lectures/es6concepts.js
@@ -18,13 +18,47 @@ let sum = (a, b) => {
   return { a, b };
 };
 /**rest Operator ... */
-let sum_ind = (...args) => {
+/* let sum_ind = (...args) => {
   console.log(args);
 };
-sum_ind(1, 2, 3, 4, 5);
+sum_ind(1, 2, 3, 4, 5); */
 /**spread Operator ... */
-arr2 = arr;
+/* arr2 = arr;
 let arr = [1, 2, 3];
 let arr2 = [...arr];
 arr2[0] = 50;
-console.log(arr[0]);
+console.log(arr[0]); */
+let assign_ex = () => {
+  return [1, 2, 3];
+};
+
+/* let [a, b, c] = assign_ex(); */
+//let arr_a = [1, , 3];
+//console.log(arr_a.length);
+//console.log(arr_a[1]);
+/* result="a="+a+""
+console.log(`a=${a},b=${b},c=${c}`); */
+/* let scores = [90, 60, 80];
+for (let score of scores.e) {
+  console.log(score);
+} */
+class Student {
+  constructor(name, classStd) {
+    this._name = name;
+    this.classStd = classStd;
+  }
+  get name() {
+    return this._name;
+  }
+  set name(n) {
+    this._name = n;
+  }
+  display() {
+    console.log(this._name, this.classStd);
+  }
+}
+let std1 = new Student("Kamran", "BCS");
+console.log(std1.name);
+
+let example=`hello
+i am `
